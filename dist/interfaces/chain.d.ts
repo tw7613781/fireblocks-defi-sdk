56f9186779c01fb3c873e421739adbfe340549ae
Tang Wei <tw7613781@gmail.com>
2022-03-23 22:48:24 +0800

    add dist file for vercel use

---
+++ dist/interfaces/chain.d.ts
@@ -0,0 +1,8 @@
+export declare enum Chain {
+    MAINNET = "mainnet",
+    ROPSTEN = "ropsten",
+    KOVAN = "kovan",
+    BSC = "bsc",
+    BSC_TEST = "bsc_test",
+    POLYGON = "polygon"
+}
---
+++ dist/interfaces/chain.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Chain = void 0;
+var Chain;
+(function (Chain) {
+    Chain["MAINNET"] = "mainnet";
+    Chain["ROPSTEN"] = "ropsten";
+    Chain["KOVAN"] = "kovan";
+    Chain["BSC"] = "bsc";
+    Chain["BSC_TEST"] = "bsc_test";
+    Chain["POLYGON"] = "polygon";
+})(Chain = exports.Chain || (exports.Chain = {}));
